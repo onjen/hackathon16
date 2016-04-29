ea1f3bee272437f2cda1b5abb179867f53665931
Matthias Loebach <matthias.loebach@ima-zlw-ifu.rwth-aachen.de>
2016-04-30 00:44:14 +0200

    test sqlite db

---
+++ data/importToDb.py
@@ -0,0 +1,25 @@
+import csv
+import sqlite3
+import datetime
+import time
+
+ 
+conn = sqlite3.connect('data.db')
+for i in xrange(1, 6): 
+    with open('Anlage' + str(i) + '.csv', 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        for row in reader:
+            if reader.line_num == 1:
+                continue
+            date = int(time.mktime(time.strptime(row[0], "%Y-%m-%d %H:%M:%S")))
+            print date
+            outdoor = float(row[1])
+            target = float(row[1])
+            kwh = float(row[3])
+
+            conn.execute("INSERT INTO DATA (TIME, OUTDOOR, TARGET, ENERGY) \
+                          VALUES (" + str(date) + "," + str(outdoor) + "," + str(target) + "," + str(kwh) + ")");
+
+
+
+conn.close()
